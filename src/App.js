ec043409af017239bf78552ea653b23eb75a7f0f
Unknown <frederickmorin18@gmail.com>
2018-05-06 13:26:20 +1000

    Add basic styles + header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
+import { Container } from 'reactstrap';
+// components
+import Header from './components/Header';
 
 class App extends Component {
   render() {
-    return <div className="App">Hello world!</div>;
+    return (
+      <main className="App">
+        <Container>
+          <Header />
+        </Container>
+      </main>
+    );
   }
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Row, Col } from 'reactstrap';
+// css
+import './Header.css';
+import '../assets/fonts/STARWARS/STARWARS.css';
+
+const Header = () => (
+  <header>
+    <Row>
+      <Col>
+        <h1 className="title text-center text-uppercase align-items-center mt-5">
+          Star Wars<br />Planet Guide
+        </h1>
+      </Col>
+    </Row>
+  </header>
+);
+
+export default Header;
