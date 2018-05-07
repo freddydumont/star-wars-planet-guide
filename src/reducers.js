5ed5bee7f94f2406b9cd6e27214f36aabdea6b55
Unknown <frederickmorin18@gmail.com>
2018-05-07 16:57:04 +1000

    Create residents reducer and update others

--- src/reducers.js
+++ src/reducers.js
@@ -1,8 +1,14 @@
 import { combineReducers } from 'redux';
+import {
+  FETCH_RESIDENTS_PENDING,
+  FETCH_RESIDENTS_FULFILLED,
+  FETCH_PLANETS_PENDING,
+  FETCH_PLANETS_FULFILLED,
+} from './actions';
 
 function planets(state = null, action) {
   switch (action.type) {
-    case 'FETCH_PLANETS_FULFILLED':
+    case FETCH_PLANETS_FULFILLED:
       // combine the results of all promises into a single array
       const planets = action.payload.map(page => page.data.results);
       // return an array of merged planet objects
@@ -14,14 +20,31 @@ function planets(state = null, action) {
 
 function loading(state = null, action) {
   switch (action.type) {
-    case 'FETCH_PLANETS_PENDING':
+    case FETCH_PLANETS_PENDING:
       return true;
-    case 'FETCH_PLANETS_FULFILLED':
+    case FETCH_PLANETS_FULFILLED:
       return false;
     default:
       return state;
   }
 }
 
-const reducer = combineReducers({ planets, loading });
+function residents(state = {}, action) {
+  switch (action.type) {
+    case FETCH_RESIDENTS_PENDING:
+      return {
+        ...state,
+        [action.meta]: 'loading',
+      };
+    case FETCH_RESIDENTS_FULFILLED:
+      return {
+        ...state,
+        [action.meta]: action.payload.map(resident => resident.data.name),
+      };
+    default:
+      return state;
+  }
+}
+
+const reducer = combineReducers({ planets, loading, residents });
 export default reducer;
