3b3579151ed685c00848cedeb4ce1fec6b0f9b7b
Unknown <frederickmorin18@gmail.com>
2018-05-10 23:53:36 +1000

    Replace promiseMiddleware with sagaMiddleware

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, compose, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import promiseMiddleware from 'redux-promise-middleware';
+import createSagaMiddleware from 'redux-saga';
 // css
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@devexpress/dx-react-grid-bootstrap4/dist/dx-react-grid-bootstrap4.css';
@@ -11,19 +11,25 @@ import 'open-iconic/font/css/open-iconic-bootstrap.min.css';
 import './index.css';
 // redux
 import reducer from './reducers';
-import { fetchPlanets } from './actions';
+import { requestPlanets } from './actions';
+import rootSaga from './sagas';
 // others
 import App from './App';
 import unregister from './registerServiceWorker';
 
+const sagaMiddleware = createSagaMiddleware();
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 let store = createStore(
   reducer,
-  composeEnhancers(applyMiddleware(promiseMiddleware()))
+  composeEnhancers(applyMiddleware(sagaMiddleware))
 );
 
+sagaMiddleware.run(rootSaga);
+
 // dispatch request to planets API asap
-store.dispatch(fetchPlanets());
+setTimeout(() => {
+  store.dispatch(requestPlanets());
+}, 500);
 
 ReactDOM.render(
   <Provider store={store}>
