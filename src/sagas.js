da2f0b73a63332ed1524eb44b5814ca2527bc440
Unknown <frederickmorin18@gmail.com>
2018-05-10 23:49:00 +1000

    Create sagas for fetching people and planets

---
+++ src/sagas.js
@@ -0,0 +1,41 @@
+import axios from 'axios';
+import { call, takeEvery, put, all } from 'redux-saga/effects';
+import { PLANETS_REQUESTED } from './actions';
+
+/**
+ * GETs all pages of a resource from SWAPI
+ * @param {string} resource The type of data requested eg. 'people'
+ * @param {number} pageCount `resource`'s amount of pages
+ */
+function fetchResource(resource, pageCount) {
+  const api = axios.create({ baseURL: `https://swapi.co/api/${resource}` });
+
+  let promisedResource = [];
+  for (let i = 0; i < pageCount; i++) {
+    promisedResource.push(api.get(`/?page=${i + 1}`));
+  }
+
+  return axios.all(promisedResource);
+}
+
+function* fetchResourceSaga(resource, pageCount) {
+  const RESOURCE = resource.toUpperCase();
+  try {
+    const receivedResource = yield call(fetchResource, resource, pageCount);
+    yield put({ type: `${RESOURCE}_RECEIVED`, payload: receivedResource });
+  } catch (error) {
+    yield put({ type: `${RESOURCE}_REQUEST_FAILED`, payload: error });
+  }
+}
+
+function* watchPlanetsRequested() {
+  yield takeEvery(PLANETS_REQUESTED, fetchResourceSaga, 'planets', 7);
+}
+
+function* watchPeopleRequested() {
+  yield takeEvery('PEOPLE_REQUESTED', fetchResourceSaga, 'people', 9);
+}
+
+export default function* rootSaga() {
+  yield all([watchPlanetsRequested(), watchPeopleRequested()]);
+}
