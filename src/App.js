9b05d92e2cf6db5c1434072406afc42aedffa130
Unknown <frederickmorin18@gmail.com>
2018-05-12 19:58:09 +1000

    Replace Button with TryAgainButton in App

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
 import React, { Component } from 'react';
-import { Container, Button } from 'reactstrap';
+import { Container } from 'reactstrap';
 import { connect } from 'react-redux';
 import { requestPlanets } from './actions';
 // components
 import Header from './components/Header';
 import ReactGrid from './components/ReactGrid';
+import TryAgainButton from './components/TryAgainButton';
 
 class App extends Component {
   render() {
+    const { planets, loading, errors, requestPlanets } = this.props;
     // if loading, present loading indicator
     let table;
-    if (this.props.loading.planets) {
+    if (loading.planets) {
       table = <p className="text-center">Searching for planets...</p>;
     } else {
       // if error, present option to try again, else display table
-      table = this.props.errors.planets ? (
+      table = errors.planets ? (
         <div className="text-center">
           <h2 className="mb-3">ERROR</h2>
           <p style={{ color: '#4bd5ee' }}>
             These are not the planets you're looking for...
           </p>
-          <Button onClick={() => this.props.requestPlanets()}>
-            Try Again?
-          </Button>
+          <TryAgainButton onClickFunction={requestPlanets} />
         </div>
       ) : (
-        <ReactGrid planets={this.props.planets} />
+        <ReactGrid planets={planets} />
       );
     }
 
