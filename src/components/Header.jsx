c0981aeaf8bea5e979c93750f396eada03e4f119
Unknown <frederickmorin18@gmail.com>
2018-05-16 20:53:02 +1000

    Replace Header.css with a styled h1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import { Row, Col } from 'reactstrap';
-// css
-import './Header.css';
+import styled from 'styled-components';
 import '../assets/fonts/STARWARS/STARWARS.css';
+
+const Title = styled.h1`
+  font-family: 'STARWARS';
+  color: #000;
+  text-shadow: -2px -2px 0 #ffd54e, -2px -1px 0 #ffd54e, -2px 0 0 #ffd54e,
+    -2px 1px 0 #ffd54e, -2px 2px 0 #ffd54e, -1px -2px 0 #ffd54e,
+    -1px -1px 0 #ffd54e, -1px 0 0 #ffd54e, -1px 1px 0 #ffd54e,
+    -1px 2px 0 #ffd54e, 0 -2px 0 #ffd54e, 0 -1px 0 #ffd54e, 0 0 0 #ffd54e,
+    0 1px 0 #ffd54e, 0 2px 0 #ffd54e, 1px -2px 0 #ffd54e, 1px -1px 0 #ffd54e,
+    1px 0 0 #ffd54e, 1px 1px 0 #ffd54e, 1px 2px 0 #ffd54e, 2px -2px 0 #ffd54e,
+    2px -1px 0 #ffd54e, 2px 0 0 #ffd54e, 2px 1px 0 #ffd54e, 2px 2px 0 #ffd54e;
+`;
 
 const Header = () => (
   <header>
     <Row>
       <Col>
-        <h1 className="title text-center text-uppercase align-items-center my-5 ">
+        <Title className="title text-center text-uppercase align-items-center my-5 ">
           Star Wars<br />Planet Guide
-        </h1>
+        </Title>
       </Col>
     </Row>
   </header>
